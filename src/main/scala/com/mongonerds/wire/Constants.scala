4d13534009149fd262e3476cb944ee2fbde36e55
Adán Sánchez de Pedro Crespo <adan@stampery.com>
2017-02-22 12:24:24 +0100

    Add remaining opcodes

--- src/main/scala/com/mongonerds/wire/Constants.scala
+++ src/main/scala/com/mongonerds/wire/Constants.scala
@@ -1,6 +1,14 @@
 package com.mongonerds.wire
 
 object OpCodes {
-  val opReply = 1
-  val opQuery = 2004
+  val opReply         = 1
+  val opMsg           = 1000
+  val opUpdate        = 2001
+  val opInsert        = 2002
+  val opQuery         = 2004
+  val opGetMore       = 2005
+  val opDelete        = 2006
+  val opKillCursor    = 2007
+  val opCommand       = 2010
+  val opCommandReply  = 2011
 }
