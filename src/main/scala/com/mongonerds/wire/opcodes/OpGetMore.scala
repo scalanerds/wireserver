5119bb18aeb068ab22974fffc282291a1bb0add7
Johann Og <kronolynx@gmail.com>
2017-02-28 17:04:06 +0100

    Implement OpGetMore

--- src/main/scala/com/mongonerds/wire/opcodes/OpGetMore.scala
+++ src/main/scala/com/mongonerds/wire/opcodes/OpGetMore.scala
@@ -1,15 +1,29 @@
 package com.mongonerds.wire.opcodes
 
-import java.nio.ByteOrder.LITTLE_ENDIAN
 
 import akka.util.ByteString
+import com.mongonerds.utils.Utils._
 import com.mongonerds.wire.{Message, MsgHeader}
-import org.bson.{BSON, BSONObject}
 
 object OpGetMore {
-  def apply(msgHeader: MsgHeader, content: Array[Byte]): OpGetMore = ???
+  def apply(msgHeader: MsgHeader, content: Array[Byte]): OpGetMore = {
+    val it = content.iterator
+    val reserved = it.getInt
+    val fullCollectionName = it.getString
+    val numberToReturn = it.getInt
+    val cursorID = it.getLong
+    new OpGetMore(msgHeader, fullCollectionName, numberToReturn, cursorID, reserved)
+  }
 }
 
-class OpGetMore(val msgHeader: MsgHeader) extends Message {
-  override def serialize: ByteString = ???
+class OpGetMore(val msgHeader: MsgHeader, fullCollectionName: String,
+                numberToReturn: Int, cursorID: Long, reserved: Int) extends Message {
+  override def serialize: ByteString = {
+    val content = msgHeader.serialize ++
+      reserved.toByteArray ++
+      fullCollectionName.toByteArray ++
+      numberToReturn.toByteArray ++
+      cursorID.toByteArray
+    ByteString((content.length + 4).toByteArray ++ content)
+  }
 }
