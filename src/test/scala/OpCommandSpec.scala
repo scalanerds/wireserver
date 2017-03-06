e583cc551580e936e56e1aad6910e94fd6385bcb
Johann Og <kronolynx@gmail.com>
2017-03-06 15:49:28 +0100

    Implement OpCommand test

--- src/test/scala/OpCommandSpec.scala
+++ src/test/scala/OpCommandSpec.scala
@@ -1,4 +1,28 @@
+import akka.util.ByteString
+import com.mongodb.util.JSON
+import com.scalanerds.wire.{Message, OpCodes}
+import com.scalanerds.wire.opcodes.OpCommand
+import org.bson.BSONObject
 import org.scalatest.{FlatSpec, Matchers}
 
 class OpCommandSpec extends FlatSpec with Matchers {
+  val opCommandByteString = ByteString(88, 0, 0, 0, 30, 0, 0, 0, 0, 0, 0, 0, -38, 7, 0, 0, 104, 111, 98, 98, 105, 101, 115, 0, 108, 105, 115, 116, 67, 111, 108, 108, 101, 99, 116, 105, 111, 110, 115, 0, 43, 0, 0, 0, 1, 108, 105, 115, 116, 67, 111, 108, 108, 101, 99, 116, 105, 111, 110, 115, 0, 0, 0, 0, 0, 0, 0, -16, 63, 3, 102, 105, 108, 116, 101, 114, 0, 5, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0)
+
+  val command: OpCommand = Message(opCommandByteString).asInstanceOf[OpCommand]
+
+  it should "have header" in {
+    val header = command.msgHeader
+    header.opCode should be(OpCodes.opCommand)
+    header.requestId should be(30)
+    header.responseTo should be(0)
+  }
+
+  it should "have metadata" in {
+    val bson = JSON.parse("{ \"listCollections\" : 1.0 , \"filter\" : { }}").asInstanceOf[BSONObject]
+    command.metadata should equal(bson)
+  }
+
+  it should "serialize" in {
+    command.serialize === opCommandByteString
+  }
 }
